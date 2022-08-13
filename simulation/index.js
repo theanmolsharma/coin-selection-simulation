f7c12748ab8711889eb6208ee33358a8bc947449
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2022-08-13 10:51:47 +0530

    simulator: reports now get saved to file

--- simulation/index.js
+++ simulation/index.js
@@ -7,8 +7,10 @@ const plugin = require('../lib/wallet/plugin');
 const {testdir, rimraf, forValue} = require('../test/util/common');
 const Amount = require('../lib/btc/amount');
 const {getMinFee} = require('../lib/protocol/policy');
+const fs = require('fs/promises');
 
 let data;
+const file = `report-${Date.now()}.txt`;
 
 const run = async (useSelectEstimate) => {
   const prefix = testdir('coinselection');
@@ -96,6 +98,14 @@ const run = async (useSelectEstimate) => {
   console.log("Total fee by Alice:", feeAlice);
   console.log("Total fee by Miner:", feeMiner);
   console.log(await alice.getBalance());
+  await fs.appendFile(file, `Total received: ${receive}\n`, err => {});
+  await fs.appendFile(file, `Total sent: ${sent}\n`, err => {});
+  await fs.appendFile(file, `Total fee: ${feeMiner + feeAlice}\n`, err => {});
+  await fs.appendFile(file, `Total fee by Alice: ${feeAlice}\n`, err => {});
+  await fs.appendFile(file, `Total fee by Miner: ${feeMiner}\n`, err => {});
+  let balance = await alice.getBalance();
+  await fs.appendFile(file, `Balance: ${balance.unconfirmed}\n`, err => {});
+  await fs.appendFile(file, `Coins:¸ ${balance.coin}\n`, err => {});
 
   await node.close();
   await rimraf(prefix);
@@ -107,11 +117,16 @@ const run = async (useSelectEstimate) => {
   assert(process.argv.length > 2, 'Please pass in simulation file');
   data = require(`./json/${process.argv[2]}`);
   console.log(`Starting simulation on ${process.argv[2]}`);
+  await fs.writeFile(file, `Starting simulation on ${process.argv[2]}\n`, {flag: 'w+'}, err => {
+  });
   for (const useSelectEstimate of [true, false]) {
-    if (useSelectEstimate)
-      console.log('Running simulation on old selection')
-    else
-      console.log('Running simulation on new selection')
+    if (useSelectEstimate) {
+      console.log('Running simulation on old selection');
+      await fs.appendFile(file, 'Running simulation on old selection\n', err => {});
+    } else {
+      console.log('Running simulation on new selection');
+      await fs.appendFile(file, 'Running simulation on new selection\n', err => {});
+    }
     await run(useSelectEstimate);
   }
 })().then(() => {
